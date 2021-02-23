b885e8bde33cfc070dcc828bd7f9ba14c62970fc
Andrea Torrecilla Manero <andrea.torrecilla@BOSONIT.LOCAL>
2021-02-23 13:42:29 +0100

    filtrado y ordenación de items

--- to-do-list/src/components/todo-list.js
+++ to-do-list/src/components/todo-list.js
@@ -47,23 +47,9 @@ export class TodoList extends LitElement {
 
   constructor() {
     super();
-    this.items = [
-        {
-          name: 'uno',
-          completed: false
-        
-        },
-        {
-          name: 'dos',
-          completed: false
-        
-        },
-        {
-          name: 'tres',
-          completed: true
-          
-        },
-    ]
+    this.items = [];
+    this.order = 'asc';
+    this.query = ''; 
   }
   render() {
     return html`
@@ -77,9 +63,72 @@ export class TodoList extends LitElement {
       </div>
 
       ${
-        this.tasks.map( item => html`<todo-item .task=${item}></todo-item>`)
+        //bindeo a un metodos del componente, cada vez que cambien las propiedades se llamará a doOrder
+        //cada vez que cambie task, order o query
+        this.getItems(this.tasks, this.query, this.order).map( item => html`<todo-item .task=${item}></todo-item>`)
       }
     `;
+  }
+
+  
+  getItems(tasks, query, order){
+    return this.doOrder(this.doFilter(tasks, query), order);
+  }
+
+  doFilter(tasks, query) {
+    console.log('dofilter')
+    //el metodo filter recibe un elemento y devuelve true o false si quiero que se quede ene el array o no
+    return tasks.filter(item => {
+      //si no tengo query quiero que queden todos los items
+      if(!query) {
+        console.log('no !query')
+        return true;
+      }
+      //si el name está en la query devuelve la posisción exacta, si es distinto de -1 es que hexiste
+      if (item.name.indexOf(query) != -1) {
+        console.log('hay indexof')        
+        return true;
+      }
+      //si no hay coincidencia con la query devuelve false
+      console.log('false')
+      return false;
+    });
+  }
+
+  setFilterAsc(){
+    console.log('setFilterAsc')
+    this.order = 'asc';
+    
+  }
+
+  setFilterDesc(){
+    console.log('setFilterdesc')
+    this.order = 'desc';
+  }
+
+  doOrder(tasks, order) {
+    console.log('doOrder', tasks, order)
+    return tasks.sort((a, b) => {
+      let nameA, nameB;
+      if(order == 'asc') {
+        nameA = a.name.toLowerCase();
+        nameB = b.name.toLowerCase();
+      } else {
+        nameB = a.name.toLowerCase();
+        nameA = b.name.toLowerCase();
+      }
+      if(nameA > nameB) {
+        return 1;
+      }
+      if(nameA < nameB) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+
+  changeFilter(e){
+    this.query = e.target.value;
   }
 
 }
